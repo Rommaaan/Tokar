514c26b2170f82c5812a11184a46bf642b482bbc
FaRomik <147040323+Rommaaan@users.noreply.github.com>
2024-04-25 15:29:42 +0300

    Create 5. 21-25.cs

---
+++ 5. 21-25.cs
@@ -0,0 +1,74 @@
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // №21
+        //    int num1 = 2;
+        //    int num2 = 3;
+        //    int cubeProductOfIntegers = CubeProductOfIntegers(num1, num2);
+        //    MessageBox.Show("Cube of Product of Integers: " + cubeProductOfIntegers);
+        //}
+
+        //static int CubeProductOfIntegers(int a, int b)
+        //{
+        //    int product = a * b;
+        //    return product * product * product;
+        //}
+
+
+        // №22
+        //    int num1 = 8;
+        //    int num2 = 2;
+        //    int cubeQuotientOfIntegers = CubeQuotientOfIntegers(num1, num2);
+        //    MessageBox.Show("Cube of Quotient of Integers: " + cubeQuotientOfIntegers);
+        //}
+        //static int CubeQuotientOfIntegers(int a, int b)
+        //{
+        //    if (b == 0)
+        //    {
+        //        MessageBox.Show("Division by zero is not allowed");
+        //        return 0;
+        //    }
+        //    int quotient = a / b;
+        //    return quotient * quotient * quotient;
+        //}
+
+
+        // №23
+        //    double num1 = 3.5;
+        //    double num2 = 2.8;
+        //    double cubeSumOfDoubles = CubeSumOfDoubles(num1, num2);
+        //    MessageBox.Show("Cube of Sum of Doubles: " + cubeSumOfDoubles);
+        //}
+        //static double CubeSumOfDoubles(double a, double b)
+        //{
+        //    return Math.Pow(a + b, 3);
+        //}
+
+
+        // №24
+        //    double num1 = 5.2;
+        //    double num2 = 3.1;
+        //    double cubeDifferenceOfDoubles = CubeDifferenceOfDoubles(num1, num2);
+        //    MessageBox.Show("Cube of Difference of Doubles: " + cubeDifferenceOfDoubles);
+        //}
+        //static double CubeDifferenceOfDoubles(double a, double b)
+        //{
+        //    return Math.Pow(a - b, 3);
+        //}
+
+
+        // №25
+        //    double num1 = 4.5;
+        //    double num2 = 1.5;
+        //    double cubeProductOfDoubles = CubeProductOfDoubles(num1, num2);
+        //    MessageBox.Show("Cube of Product of Doubles: " + cubeProductOfDoubles);
+        //}
+        //static double CubeProductOfDoubles(double a, double b)
+        //{
+        //    return Math.Pow(a * b, 3);
+        //}
+    }
+}
