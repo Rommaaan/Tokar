f0aa780ef499795490581360af9294825e6d2aac
FaRomik <147040323+Rommaaan@users.noreply.github.com>
2024-04-25 12:36:03 +0300

    Create 6-10.cs

---
+++ 6-10.cs
@@ -0,0 +1,73 @@
+public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // №6
+            //    double num1 = 10.5;
+            //    double num2 = 3.2;
+            //    double difference = Subtract(num1, num2);
+            //    MessageBox.Show("Difference: " + difference);
+            //}
+            //static double Subtract(double a, double b)
+            //{
+            //    return a - b;
+            //}
+
+
+            // №7
+            //    double num1 = 2.5;
+            //    double num2 = 4.8;
+            //    double product = Multiply(num1, num2);
+            //    MessageBox.Show("Product: " + product);
+            //}
+            //static double Multiply(double a, double b)
+            //{
+            //    return a * b;
+            //}
+
+
+            // №8
+            //    double num1 = 15.6;
+            //    double num2 = 3.3;
+            //    double quotient = Divide(num1, num2);
+            //    MessageBox.Show("Quotient: " + quotient);
+            //}
+            //static double Divide(double a, double b)
+            //{
+            //    if (b == 0)
+            //    {
+            //        MessageBox.Show("Division by zero is not allowed");
+            //        return 0;
+            //    }
+            //    else
+            //    {
+            //        return a / b;
+            //    }
+
+
+            // №9
+            //    int num = 7;
+            //    int square = Square(num);
+            //    MessageBox.Show("Square: " + square);
+            //}
+            //static int Square(int a)
+            //{
+            //    return a * a;
+            //}
+
+
+            // №10
+            //    int num1 = 4;
+            //    int num2 = 5;
+            //    int sumSquare = SquareOfSum(num1, num2);
+            //    MessageBox.Show("Square of Sum: " + sumSquare);
+            //}
+            //static int SquareOfSum(int a, int b)
+            //{
+            //    int sum = a + b;
+            //    return sum * sum;
+            //}
+        }
+    }
